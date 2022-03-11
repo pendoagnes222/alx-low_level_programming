a384f63797785de31cb2afb246c5cac61aaa16d8
dianakyalo202 <99383447+dianakyalo202@users.noreply.github.com>
2022-03-11 16:13:29 +0300

    My first variable

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,32 +1,25 @@
-/*
- * the program applies the if statement to test for conditions
- */
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 
 /**
  * main - Entry point
- * Return: returns 0
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n > 0)
-	{
 		printf("%d is positive\n", n);
-	}
-	else if (n < 0)
-	{
-		printf("%d is negative\n", n);
-	}
-	else
-	{
+	else if (n == 0)
 		printf("%d is zero\n", n);
-	}
+	else
+		printf("%d is negative\n", n);
+
 	return (0);
 }
