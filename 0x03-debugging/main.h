b1303982e07246bc6afdb8f72558251d8da13904
dianakyalo202 <99383447+dianakyalo202@users.noreply.github.com>
2022-03-16 22:46:11 +0300

    Multiple file debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,7 +5,6 @@ i#ifndef MAIN_H
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
-int convert_day(int month, int day);
-void print_remaining_days(int month, int day, int year);
+void positive_or_negative(int a, int b, int c);
 
 #endif /* MAIN_H */
