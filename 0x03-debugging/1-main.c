02fa28d2ca95f5d8f7d9543bed8a774843094e77
dianakyalo202 <99383447+dianakyalo202@users.noreply.github.com>
2022-03-16 19:53:51 +0300

    Multiple file debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,4 +1,4 @@
-#include <stdio.h
+#include <stdio.h>
 
 /**
 * main - causes an infinite loop
@@ -11,14 +11,14 @@ int main(void)
 
         printf("Infinite loop incoming :(\n");
 
-	i = 0;
+        i = 0;
 
-        /*while (i < 10*/
+        /*while (i < 10)*/
         /*{*/
-	/*putchar(i);*/
-	/*}*/
+                /*putchar(i)*/
+       /*}*/
 
-       printf("Infinite loop avoided! \\o/\n");
+        printf("Infinite loop avoided! \\o/\n");
 
-       return (0);
+        return (0);
 }
