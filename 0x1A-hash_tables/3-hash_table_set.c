559c590dd9911657283470b22dc56b3927a27252
dianakyalo202 <dianakyalo202@gmai.com>
2022-07-02 21:43:20 +0300

    Hash tables

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,71 +1,66 @@
 #include "hash_tables.h"
 
 /**
- * helper_hash_djb2 - implementation of the djb2 algorithm
- * @str: string used to generate hash value
+ * free_node - Free a node.
+ * @node: Node to free.
  *
- * Return: hash value
+ * Return: Void.
  */
-unsigned long int helper_hash_djb2(const char *str)
+void free_node(hash_node_t *node)
 {
-	unsigned long int hash;
-	int c;
-
-	hash = 5381;
-	while ((c = *str++))
-	{
-		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-	}
-	return (hash);
-}
-
-
-/**
- * helper_key_index - gives the index of the key
- * @key: key to get index for
- * @size: size of the array of the hash table
- * Return: index of the key
- */
-unsigned long int helper_key_index(const char *key, unsigned long int size)
-{
-	unsigned long int hash_value = helper_hash_djb2(key);
-	unsigned long int index;
-
-	index = hash_value % size;
-	return (index);
+	free(node->key);
+	free(node->value);
+	free(node);
 }
 
 /**
- * hash_table_set - adds an element to the hash table
- * @ht: the hash table
- * @key: the key
- * @value: value pair
- * Return: an integer
+ * hash_table_set - Set a value in the hash table.
+ * @ht: Hash table.
+ * @key: Key to be indexed.
+ * @value: Value to set in the hash table.
+ *
+ * Return: 1 if works, 0 if doesn't.
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_node, *temp;
 	unsigned long int index;
+	hash_node_t *new_node, *current;
 
+	if (strcmp(key, "") == 0 || key == NULL || ht == NULL)
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
 	new_node = malloc(sizeof(hash_node_t));
-	if (!new_node)
+	if (new_node == NULL)
 		return (0);
-
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
+	new_node->key = strdup((char *)key);
+	new_node->value = strdup((char *)value);
 	new_node->next = NULL;
-	if (!ht)
-		return (0);
-	index = helper_key_index(key, ht->size);
-	if (!ht->array)
-		return (0);
-	if (!ht->array[index])
-	{
+	if (ht->array[index] == NULL)
 		ht->array[index] = new_node;
-		return (1);
+	else
+	{
+		current = ht->array[index];
+		if (strcmp(current->key, key) == 0)
+		{
+			new_node->next = current->next;
+			ht->array[index] = new_node;
+			free_node(current);
+			return (1);
+		}
+		while (current->next != NULL && strcmp(current->next->key, key) != 0)
+		{ current = current->next;
+		}
+		if (strcmp(current->key, key) == 0)
+		{
+			new_node->next = current->next->next;
+			free_node(current->next);
+			current->next = new_node;
+		}
+		else
+		{
+			new_node->next = ht->array[index];
+			ht->array[index] = new_node;
+		}
 	}
-	temp = ht->array[index];
-	new_node->next = temp;
-	ht->array[index] = new_node;
 	return (1);
 }
